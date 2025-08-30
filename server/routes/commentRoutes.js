7d259c03975ea14761e95fe01b79e9c0ef035270
brukzan47 <151669263+brukzan47@users.noreply.github.com>
2025-08-30 23:37:23 +0300

    Update commentRoutes.js

--- server/routes/commentRoutes.js
+++ server/routes/commentRoutes.js
@@ -1,6 +1,6 @@
 import express from "express";
 import Comment from "../models/comment.js";
-import Post from "../models/Post.js";
+import Post from "../models/post.js";
 import { authRequired } from "../middleware/auth.js";
 
 const router = express.Router();
